8506d2b79ffe7eaa6fce724d41a53af2ec6038e6
te-sh <admire.japan@gmail.com>
2024-07-13 01:26:47 +0900

    change directory in compile-with-lib.sh

--- code/script/compile-with-lib.sh
+++ code/script/compile-with-lib.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
 LIB=$(cd $(dirname $0)/../lib; pwd)
-CRYSTAL_PATH=`crystal env CRYSTAL_PATH`:$LIB crystal $@
+
+ARGS=()
+for ARG in $@
+do
+    if [[ $ARG == *.cr ]]; then
+        ARGS+="$(basename $ARG) "
+        DIR=$(cd $(dirname $ARG) && pwd)
+    else
+        ARGS+="$ARG "
+    fi
+done
+
+echo "start"
+cd $DIR
+CRYSTAL_PATH=`crystal env CRYSTAL_PATH`:$LIB crystal ${ARGS[@]}
