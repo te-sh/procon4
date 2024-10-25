f4636ce77ea6bd89dff6262b2a0670a0c9e88465
te-sh <admire.japan@gmail.com>
2024-10-25 18:29:00 +0900

    add bit_subsets and bit_supersets

--- code/lib/procon/bit.cr
+++ code/lib/procon/bit.cr
@@ -23,5 +23,56 @@ struct Int
   def reset_bit(i : Int)
     self & ~(self.class.new(1) << i)
   end
+
+  #
+  # ビットの部分集合の Enumerator を返します
+  #
+  def bit_subsets
+    BitSubsetsEnumerator.new(self)
+  end
+
+  #
+  # ビットの包含集合の Enumerator を返します
+  #
+  def bit_supersets(m : Int32)
+    BitSupersetsEnumerator.new(self, m)
+  end
+end
+
+#
+# ビットの部分集合を列挙する Enumerator です
+#
+class BitSubsetsEnumerator(T)
+  include Enumerable(T)
+
+  def initialize(@n : T)
+  end
+
+  def each(&)
+    n = @n
+    while n >= 0
+      n &= @n
+      yield n
+      n -= 1
+    end
+  end
+end
+
+#
+# ビットの包含集合を列挙する Enumerator です
+#
+class BitSupersetsEnumerator(T)
+  include Enumerable(T)
+
+  def initialize(@n : T, @m : Int32)
+  end
+
+  def each(&)
+    n = @n
+    while n < (T.new(1) << @m)
+      yield n
+      n = (n + 1) | @n
+    end
+  end
 end
 # ::::::::::::::::::::
--- code/spec/procon/bit_spec.cr
+++ code/spec/procon/bit_spec.cr
@@ -17,5 +17,22 @@ module IntSpec
       it { 5.reset_bit(1).should eq 5 }
       it { 5.reset_bit(2).should eq 1 }
     end
+
+    describe "#bit_subsets" do
+      it do
+        0b10101.bit_subsets.to_a.should eq [
+          0b10101, 0b10100, 0b10001, 0b10000,
+          0b00101, 0b00100, 0b00001, 0b00000
+        ]
+      end
+    end
+
+    describe "#bit_supersets" do
+      it do
+        0b0110.bit_supersets(4).to_a.should eq [
+          0b0110, 0b0111, 0b1110, 0b1111
+        ]
+      end
+    end
   end
 end
