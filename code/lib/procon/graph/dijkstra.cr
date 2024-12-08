ae17f5b7d2f8a299d95d299e25c9d486f454b69e
te-sh <admire.japan@gmail.com>
2024-12-08 10:44:27 +0900

    refactoring dijkstra and tree

--- code/lib/procon/graph/dijkstra.cr
+++ code/lib/procon/graph/dijkstra.cr
@@ -6,81 +6,61 @@ require "procon/data_structure/heap"
 #
 # Dijkstra 法を実装します
 #
-module Dijkstra(T)
+class Dijkstra(T)
   alias Node = GraphW::Node
   alias Edge = GraphW::Edge
-
+  
   #
-  # Runner を作成するファクトリーです
+  # コンストラクタ
   #
-  struct Factory(T)
-    #
-    # コンストラクタ
-    #
-    def initialize(@g : GraphW(T))
-    end
-
-    #
-    # Dijkstra 法を実行する Runner を返します
-    #
-    def run(s : Node)
-      Runner(T).new(@g).run(s)
-    end
+  def initialize(@g : GraphW(T))
+    size = @g.size
+    @dist = Array.new(size, @g.inf)
+    @prev = Array.new(size, GraphW::DUMMY_NODE)
   end
-
+  
   #
-  # Dijkstra 法を実行する Runner です
+  # s から各頂点への最短距離を計算します
   #
-  class Runner(T)
-    #
-    # コンストラクタ
-    #
-    def initialize(@g : GraphW(T))
-      size = @g.size
-      @dist = Array.new(size, @g.inf)
-      @prev = Array.new(size, GraphW::DUMMY_NODE)
-    end
-
-    #
-    # s から各頂点への最短距離を計算します
-    #
-    def run(s : Node)
-      @dist[s] = T.additive_identity
-      se = Edge.new(GraphW::DUMMY_NODE, s, T.new(0))
-      h = Heap.new([se]) { |a, b| a.wt <=> b.wt }
-
-      until h.empty?
-        e = h.pop
+  def run(s : Node)
+    @dist[s] = T.additive_identity
+    se = Edge.new(GraphW::DUMMY_NODE, s, T.new(0))
+    h = Heap.new([se]) { |a, b| a.wt <=> b.wt }
+    
+    until h.empty?
+      e = h.pop
       
-        next if @prev[e.dst] != GraphW::DUMMY_NODE
-        @prev[e.dst] = e.src
+      next if @prev[e.dst] != GraphW::DUMMY_NODE
+      @prev[e.dst] = e.src
       
-        @g[e.dst].each do |f|
-          w = e.wt + f.wt
-          if w < @dist[f.dst]
-            @dist[f.dst] = w
-            h.push(Edge.new(f.src, f.dst, w))
-          end
+      @g[e.dst].each do |f|
+        w = e.wt + f.wt
+        if w < @dist[f.dst]
+          @dist[f.dst] = w
+          h.push(Edge.new(f.src, f.dst, w))
         end
       end
-
-      self
     end
 
-    #
-    # 指定された頂点から各頂点への距離を配列で返します
-    #
-    getter dist : Array(T)
-
-    # ---------- private methods
-
-    @prev : Array(Node)
+    self
   end
+  
+  #
+  # 指定された頂点から各頂点への距離を配列で返します
+  #
+  getter dist : Array(T)
+  
+  # ---------- private methods
+  
+  @prev : Array(Node)
 end
 
 class GraphW(T)
+  #
+  # s から各頂点への最短距離を計算します
+  #
   def dijkstra(s : Node)
-    Dijkstra::Factory.new(self).run(s)
+    Dijkstra.new(self).run(s)
   end
 end
 
--- code/lib/procon/graph/tree/tree.cr
+++ code/lib/procon/graph/tree/tree.cr
@@ -4,104 +4,85 @@ require "procon/graph/graph"
 require "procon/graph/dfs"
 
 #
-# 木を実装します
+# 木を表します
+# 
 #
-module Tree
+class Tree
   alias Node = Graph::Node
-
   #
-  # Parser を作成するファクトリーです
+  # コンストラクタ
   #
-  struct Factory
-    #
-    # コンストラクタ
-    #
-    def initialize(@g : Graph)
-    end
+  def initialize(@g : Graph)
+    @parent = Array.new(size, 0)
+    @children = Array.new(size) { [] of Node }
+    @depth = Array.new(size, -1)
+    @dfs_order = [] of Node
+    @subtree_size = Array.new(size, 1)
+  end
   
-    #
-    # 木のパーサを返します
-    #
-    def parse(root : Node)
-      Parser.new(@g, root).parse
-    end
-  end
-
   #
-  # 木を作成する Parser です
+  # root を根とする木を作成します
   #
-  class Parser
-    #
-    # コンストラクタ
-    #
-    def initialize(@g : Graph, @root : Node)
-      @parent = Array.new(size, 0)
-      @children = Array.new(size) { [] of Node }
-      @depth = Array.new(size, -1)
-      @dfs_order = [] of Node
-      @subtree_size = Array.new(size, 1)
+  def build(root : Node)
+    @root = root
+    size = @g.size
+    
+    @g.dfs(root).each do |u, p|
+      @parent[u] = p
+      @children[p] << u if u != @root
+      @depth[u] = @depth[p] + 1
+      @dfs_order << u
     end
-
-    #
-    # root を根とする木を作成します
-    #
-    def parse
-      size = @g.size
-
-      @g.dfs(@root).each do |u, p|
-        @parent[u] = p
-        @children[p] << u if u != @root
-        @depth[u] = @depth[p] + 1
-        @dfs_order << u
-      end
-
-      @dfs_order.reverse_each do |u|
-        @subtree_size[@parent[u]] += @subtree_size[u] if u != @root
-      end
-
-      self
+    
+    @dfs_order.reverse_each do |u|
+      @subtree_size[@parent[u]] += @subtree_size[u] if u != @root
     end
-
-    #
-    # 頂点数を返します
-    #
-    delegate size, to: @g
     
-    #
-    # 根を返します
-    #
-    getter root : Node
-    
-    #
-    # 木を DFS でたどったときの頂点の配列を返します
-    #
-    getter dfs_order : Array(Node)
-    
-    #
-    # 親の頂点の配列を返します
-    #
-    getter parent : Array(Node)
-    
-    #
-    # 子の頂点を配列の配列で返します
-    #
-    getter children : Array(Array(Node))
-    
-    #
-    # 頂点深さを配列で返します
-    #
-    getter depth : Array(Int32)
-    
-    #
-    # 部分木の頂点数を配列で返します
-    #
-    getter subtree_size : Array(Int32)
+    self
   end
+  
+  #
+  # 頂点数を返します
+  #
+  delegate size, to: @g
+  
+  #
+  # 根を返します
+  #
+  getter! root : Node
+  
+  #
+  # 木を DFS でたどったときの頂点の配列を返します
+  #
+  getter dfs_order : Array(Node)
+  
+  #
+  # 親の頂点の配列を返します
+  #
+  getter parent : Array(Node)
+  
+  #
+  # 子の頂点を配列の配列で返します
+  #
+  getter children : Array(Array(Node))
+  
+  #
+  # 頂点深さを配列で返します
+  #
+  getter depth : Array(Int32)
+  
+  #
+  # 部分木の頂点数を配列で返します
+  #
+  getter subtree_size : Array(Int32)
 end
 
 class Graph
+  #
+  # root を根とする木を作成します
+  # 
   def tree(root : Node)
-    Tree::Factory.new(self).parse(root)
+    Tree.new(self).build(root)
   end
 end
 
